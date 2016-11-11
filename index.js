7fe701ac59e530170c1adb82a44ee9a6c0939256
federicoghedinastailamedia <federico.ghedina@stailamedia.com>
2016-11-11 13:24:22 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+require('malta').checkDeps('uglifycss');
+
+var uglify_css = require("uglifycss"),
+	path = require('path'),
+	fs = require('fs');
+	
+function malta_css_uglify(o, options) {
+
+	var self = this,
+		start = new Date(),
+		msg;
+
+	options = options || {};
+	options.maxLineLen = options.maxLineLen || 500;
+	options.expandVars = options.expandVars || true;
+	options.uglyComments = options.uglyComments || false;
+	options.cuteComments = options.cuteComments || true;
+
+	o.name = o.name.replace(/\.css$/, '.min.css');
+
+	o.content = uglify_css.processString(o.content, options);
+
+	return function (solve, reject){
+		fs.writeFile(o.name, o.content, function(err) {
+			if (err == null) {
+				msg = 'plugin ' + path.basename(__filename) + ' wrote ' + o.name + ' (' + self.getSize(o.name) + ')';
+			} else {
+				console.log('[ERROR] uglifycss says:');
+				console.dir(err);
+				self.stop();
+			}
+			solve(o);
+			self.notifyAndUnlock(start, msg);
+		});	
+	};
+}
+malta_css_uglify.ext = ['css', 'less', 'scss'];
+module.exports = malta_css_uglify;
