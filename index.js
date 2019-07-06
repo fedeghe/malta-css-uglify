67399f77ee1fee3e9a5b54e71f3a422e5dc09491
fedeghe <fedeghe@gmail.com>
2019-07-06 22:55:00 +0200

    reject set

--- index.js
+++ index.js
@@ -1,36 +1,38 @@
 require('malta').checkDeps('uglifycss');
 
 var uglify_css = require("uglifycss"),
-	path = require('path'),
-	fs = require('fs');
-	
+    path = require('path'),
+    fs = require('fs');
+
 function malta_css_uglify(o, options) {
 
-	var self = this,
-		start = new Date(),
-		msg = "",
-		pluginName = path.basename(path.dirname(__filename));
+    var self = this,
+        start = new Date(),
+        msg = "",
+        pluginName = path.basename(path.dirname(__filename));
 
-	options = options || {};
-	options.maxLineLen = options.maxLineLen || 500;
-	options.expandVars = options.expandVars || true;
-	options.uglyComments = options.uglyComments || false;
-	options.cuteComments = options.cuteComments || true;
+    options = options || {};
+    options.maxLineLen = options.maxLineLen || 500;
+    options.expandVars = options.expandVars || true;
+    options.uglyComments = options.uglyComments || false;
+    options.cuteComments = options.cuteComments || true;
 
-	try {
-		o.content = uglify_css.processString(o.content, options);
-	} catch (err) {
-		self.doErr(err, o, pluginName);
-	}
+    try {
+        o.content = uglify_css.processString(o.content, options);
+    } catch (err) {
+        self.doErr(err, o, pluginName);
+    }
 
-	return function (solve, reject){
-		fs.writeFile(o.name, o.content, function(err) {
-			err && self.doErr(err, o, pluginName);
-			msg = 'plugin ' + pluginName.white() + ' wrote ' + o.name + ' (' + self.getSize(o.name) + ')';
-			solve(o);
-			self.notifyAndUnlock(start, msg);
-		});	
-	};
+    return function (solve, reject) {
+        fs.writeFile(o.name, o.content, function (err) {
+            err && self.doErr(err, o, pluginName);
+            msg = 'plugin ' + pluginName.white() + ' wrote ' + o.name + ' (' + self.getSize(o.name) + ')';
+            err
+                ? reject(`Uglifycation error:\n${err}`)
+                : solve(o);
+            self.notifyAndUnlock(start, msg);
+        });
+    };
 }
 malta_css_uglify.ext = ['css', 'less', 'scss'];
 module.exports = malta_css_uglify;
